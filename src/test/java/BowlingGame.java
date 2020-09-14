130c34d72f1911d7eb9be405086fad43f6251894
Anson <1072476988@qq.com>
2020-09-14 21:07:36 +0800

    [lingzhen] return 0 when given every roll is 0.

---
+++ src/test/java/BowlingGame.java
@@ -0,0 +1,8 @@
+public class BowlingGame {
+    public int scoring() {
+        return 0;
+    }
+
+    public void roll(int i) {
+    }
+}
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,2 +1,20 @@
-public class BowlingGameTest {
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class BowlingGameTest {
+    private BowlingGame bowlingGame;
+    @BeforeEach
+    void resetGame(){
+        bowlingGame = new BowlingGame();
+    }
+
+    @Test
+    void should_return_0_when_scoring_given_every_roll_is_0(){
+        for (int rollIndex = 0; rollIndex<20;rollIndex++){
+            bowlingGame.roll(0);
+        }
+        int score = bowlingGame.scoring();
+        assertEquals(score,0);
+    }
 }
