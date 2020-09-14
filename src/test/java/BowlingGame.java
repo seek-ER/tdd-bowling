cf352019ffe4fc14f8463941925a524ec880457d
Anson <1072476988@qq.com>
2020-09-14 21:15:19 +0800

    [lingzhen] return the sum of all rolls when given every roll is common like 3.

--- src/test/java/BowlingGame.java
+++ src/test/java/BowlingGame.java
@@ -1,8 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class BowlingGame {
+    private List<Integer> scores = new ArrayList<>();
     public int scoring() {
-        return 0;
+        return scores.stream().mapToInt(number -> number).sum();
     }
 
-    public void roll(int i) {
+    public void roll(int score) {
+        scores.add(score);
     }
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -17,4 +17,12 @@ class BowlingGameTest {
         int score = bowlingGame.scoring();
         assertEquals(score,0);
     }
+
+    @Test
+    void should_sum_all_rolls_when_scoring_given_every_roll_is_common_as_3(){
+        for (int rollIndex = 0; rollIndex < 20; rollIndex++) {
+            bowlingGame.roll(3);
+        }
+        assertEquals(bowlingGame.scoring(),60);
+    }
 }
